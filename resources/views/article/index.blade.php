df705f3c7375b0f13d7678c69d1259ed4ea55cef
HAN ZAW HTET <hanzawhtet317@gmail.com>
2023-02-07 23:32:28 +0630

    Create Laravel project (blog-app)

---
+++ resources/views/article/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Articles</title>
+</head>
+<body>
+    <main>
+        <h1>Article List</h1>
+        <hr>
+        <ul>
+            <li>Article 1</li>
+            <li>Article 2</li>
+            <li>Article 3</li>
+            <li>Article 4</li>
+            <li>Article 5</li>
+            <li>Article 6</li>
+            <li>Article 7</li>
+        </ul>
+    </main>
+</body>
+</html>
